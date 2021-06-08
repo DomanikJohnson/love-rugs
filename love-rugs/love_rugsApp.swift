508a9b3ecf4f0c575687ab88b388ddeb3c95d135
SwiftDom <domanik.johnson@icloud.com>
2021-06-08 18:30:32 -0400

    Initial Commit

---
+++ love-rugs/love_rugsApp.swift
@@ -0,0 +1,17 @@
+//
+//  love_rugsApp.swift
+//  love-rugs
+//
+//  Created by Domanik Johnson on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct love_rugsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
